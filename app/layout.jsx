f6dbaddd20f561b233dbe1098ced7b78706c7cc1
Thanh Hoang-Minh <hmthanhgm@gmail.com>
2025-03-16 01:43:10 +0700

    update

---
+++ app/layout.jsx
@@ -0,0 +1,29 @@
+import "./globals.css"
+
+import { ThemeProvider } from "next-themes"
+import Header from "@/components/layout/header"
+import Body from "@/components/layout/body"
+import Footer from "@/components/layout/footer"
+
+export const metadata = {
+    title: "MotionProtocol",
+    description: "MotionProtocol",
+}
+
+export default function RootLayout({ children }) {
+    return (
+        <html lang="en" suppressHydrationWarning>
+            <body className="openhuman-banner-hidden">
+                <div dir="ltr">
+                    <Header />
+                    <Body>
+                        <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
+                            {children}
+                        </ThemeProvider>
+                    </Body>
+                    <Footer />
+                </div>
+            </body>
+        </html>
+    )
+}
